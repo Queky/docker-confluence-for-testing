c2418666737c1c8253e0a746b6bc33e2932976b9
Iñaki Sánchez <inakisanchez_92@hotmail.com>
2019-10-07 23:24:16 +0200

    refactored files

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export CONFLUENCE_VERSION=$(curl -s https://marketplace.atlassian.com/rest/2/applications/confluence/versions/latest | jq -r '.version')
-echo "$CONFLUENCE_VERSION"
+export JIRA_VERSION=$(curl -s https://marketplace.atlassian.com/rest/2/applications/confluence/versions/latest | jq -r '.version')
+echo "$JIRA_VERSION"
